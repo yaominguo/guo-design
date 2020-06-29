b7d6cf366debb32190cef4864e01dd1ef63051a6
yaominguo <missgmy@yahoo.com>
2020-06-29 15:59:20 +0800

    添加Menu组件基本单元测试

---
+++ src/components/Menu/menu.test.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
+import Menu, { MenuProps } from './menu'
+import MenuItem from './menuitem'
+
+const testProps: MenuProps = {
+  defaultIndex: 0,
+  onSelect: jest.fn(),
+  className: 'test',
+}
+const testVerticalProps: MenuProps = {
+  defaultIndex: 0,
+  mode: 'vertical',
+}
+const generateMenu = (props: MenuProps) => {
+  return (
+    <Menu {...props}>
+      <MenuItem index={0}>active</MenuItem>
+      <MenuItem disabled index={1}>disabled</MenuItem>
+      <MenuItem index={2}>other</MenuItem>
+    </Menu>
+  )
+}
+
+let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement
+describe('test Menu & MenuItem component', () => {
+  beforeEach(() => {
+    wrapper = render(generateMenu(testProps))
+    menuElement = wrapper.getByTestId('test-menu')
+    activeElement = wrapper.getByText('active')
+    disabledElement = wrapper.getByText('disabled')
+  })
+  it('should render correct Menu & MenuItem base on default props', () => {
+    expect(menuElement).toBeInTheDocument()
+    expect(menuElement).toHaveClass('guo-menu test')
+    expect(menuElement.getElementsByTagName('li').length).toEqual(3)
+    expect(activeElement).toHaveClass('menu-item is-active')
+    expect(disabledElement).toHaveClass('menu-item is-disabled')
+  })
+  it('should change active index & call the right callback when trigger click events', () => {
+    const thirdItem = wrapper.getByText('other')
+    fireEvent.click(thirdItem)
+    expect(thirdItem).toHaveClass('is-active')
+    expect(activeElement).not.toHaveClass('is-active')
+    expect(testProps.onSelect).toHaveBeenCalledWith(2)
+    fireEvent.click(disabledElement)
+    expect(disabledElement).not.toHaveClass('is-active')
+    expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+  })
+  it('should render vertical mode when mode is set to vertical', () => {
+    cleanup()
+    const wrapper = render(generateMenu(testVerticalProps))
+    const menuElement = wrapper.getByTestId('test-menu')
+    expect(menuElement).toHaveClass('menu-vertical')
+  })
+})
