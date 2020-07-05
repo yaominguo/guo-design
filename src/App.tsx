214070d5335997c981ec4983aa39175f12675b7b
yaominguo <missgmy@yahoo.com>
2020-07-05 20:07:18 +0800

    添加Icon组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import Menu from './components/Menu/menu'
-import MenuItem from './components/Menu/menuitem'
-import SubMenu from './components/Menu/subMenu'
+import Icon from './components/Icon/icon'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Menu defaultOpenSubMenus={['0']} defaultIndex='0' mode="vertical" onSelect={(index) => alert(index)}>
-        <SubMenu title="sub-menu">
-          <MenuItem>menu1</MenuItem>
-          <MenuItem disabled>menu2</MenuItem>
-        </SubMenu>
-        <MenuItem>menu3</MenuItem>
-      </Menu>
+      <Icon icon="arrow-right" theme="primary" size="10x" />
     </div>
   );
 };
